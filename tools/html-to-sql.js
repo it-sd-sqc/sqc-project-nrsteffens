0f26f417e49c8aef87f537be0576325806c06deb
nrsteffes <nsteffens@student.cvtc.edu>
2023-10-04 15:06:15 -0500

    Making changes to html to sql

--- tools/html-to-sql.js
+++ tools/html-to-sql.js
@@ -1,31 +1,70 @@
 // Dependencies ////////////////////////////////////////////
-import { strict as assert } from 'node:assert'
-import { closeSync, openSync, readFileSync, writeFileSync }
+import { closeSync, openSync, readFileSync, writeFile, writeFileSync }
   from 'node:fs'
 import { parse } from 'node-html-parser'
 
-// This module uses the CommonJS module format, so we need
-// to import it differently.
-import pkg from 'svgoban'
-const { serialize } = pkg
 
 // Configuration ///////////////////////////////////////////
 const srcPath = 'data/book.html'
 const dstPath = 'docs/generated-schema.sql'
+const chapterIds = [
+  '#GR_Chapter_1',
+  '#GR_Chapter_2',
+  '#GR_Chapter_3',
+  '#GR_Chapter_4',
+  '#GR_Chapter_5',
+  '#GR_Chapter_6',
+  '#GR_Chapter_7',
+  '#GR_Chapter_8'
+]
 
 const sqlHeader = `DROP TABLE IF EXISTS chapter;
 
 CREATE TABLE chapter (
-    id CHAPTER PRIMARY KEY,
-    body TEXT NOT NULL
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL
   );`
 
-const insertChapterSql = `INSERT INTO words (chapter, body) VALUES`
+const insertChapterSql = `INSERT INTO chapters (title, body) VALUES`
 
-const gobanConfig = {
-    size: 19,
-    theme: 'classic',
-    coordSystem: 'A1',
-    noMargin: false,
-    hideMargin: false
+const extractTitle = function (root, id) {
+  const title = root.querySelector(`#${id}`).text
+  return title;
+}
+
+
+
+// Conversion ////////////////////////////////////////////
+
+
+const src = readFileSync(srcPath, 'utf8');
+const domRoot = parse(src)
+
+const chapters = []
+
+chapterIds.forEach (
+  (id) => {
+
+    // Extract title
+    const title = extractTitle(domRoot, id)
+
+    chapterIds.push({
+      title
+    })
   }
+)
+
+// Output the data as SQL.
+const fd = openSync(dstPath, 'w')
+writeFileSync(fd, sqlHeader)
+writeFileSync(fd, `('${chapters[0].index()}', '${chapters[0].title}')`)
+
+closeSync(fd)
+console.log(domRoot);
+
+
+
+
+
+  
+
