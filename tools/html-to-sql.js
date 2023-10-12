bb5e5a0b3a9469d247547703cb93b48b0ab3c746
Andrew Thayer <athayer2@cvtc.edu>
2023-10-12 12:35:09 -0500

    removed unecessary code copied from example project

--- tools/html-to-sql.js
+++ tools/html-to-sql.js
@@ -4,11 +4,6 @@ import { closeSync, openSync, readFileSync, writeFileSync }
   from 'node:fs'
 import { parse } from 'node-html-parser'
 
-// This module uses the CommonJS module format, so we need
-// to import it differently.
-import pkg from 'svgoban'
-const { serialize } = pkg
-
 // Configuration ///////////////////////////////////////////
 const srcPath = 'data/book.html'
 const dstPath = 'docs/generated-schema.sql'
