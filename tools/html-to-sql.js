91feebc44586c682827601503340b6deac18d24d
nrsteffes <nsteffens@student.cvtc.edu>
2023-10-20 19:57:37 -0500

    Made changes in order for html-to-sql.js to generate sql schema

--- tools/html-to-sql.js
+++ tools/html-to-sql.js
@@ -6,41 +6,38 @@ import { parse } from 'node-html-parser'
 
 // Configuration ///////////////////////////////////////////
 const srcPath = 'C:/Users/nstef/Documents/CVTC/SQC/sqc-project-nrsteffens/data/book.html'
-const dstPath = 'docs/generated-schema.sql'
-const chapterIds = [
-  '#GR_Chapter_1',
-  '#GR_Chapter_2',
-  '#GR_Chapter_3',
-  '#GR_Chapter_4',
-  '#GR_Chapter_5',
-  '#GR_Chapter_6',
-  '#GR_Chapter_7',
-  '#GR_Chapter_8'
-]
-
-const sqlHeader = `DROP TABLE IF EXISTS chapter;
-
-CREATE TABLE chapter (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL
-  );`
-
-const insertChapterSql = `INSERT INTO chapters (title, body) VALUES`
-
-const extractTitle = function (root, id) {
-  const title = root.querySelector(`#${id}`).text
-  return title;
-}
-
-
-
-// Conversion ////////////////////////////////////////////
-
+const dstPath = 'C:/Users/nstef/Documents/CVTC/SQC/sqc-project-nrsteffens/data/generated-schema.sql'
 
 const src = readFileSync(srcPath, 'utf8');
 const domRoot = parse(src)
 
-console.log(domRoot);
+const chapterClass = domRoot.querySelectorAll('.pginternal > span')
+
+
+
+const sqlHeader = `DROP TABLE IF EXISTS chapters;
+
+CREATE TABLE chapters (
+    id SERIAL PRIMARY KEY,
+    chapter text not null
+  );`
+
+
+const insertStatements = [];
+
+for (let i = 0; i < chapterClass.length; i++) {
+  const insertChapterSql = `INSERT INTO chapters (chapter)  VALUES ('${chapterClass[i].innerText}');` 
+
+  insertStatements.push(insertChapterSql);
+}
+
+const sqlInsertStatements = insertStatements.join('\n')
+
+writeFileSync(dstPath, sqlHeader, 'utf8')
+writeFileSync(dstPath, sqlInsertStatements, { encoding: 'utf8', flag: 'a' })
+
+
+
 
 
 
